e97bf9435971879354f4032c6fd1ee3deb1a46e2
jeonyoungho <yhjun1000@gmail.com>
2025-05-03 15:05:40 +0900

    init project

---
+++ src/main/java/org/example/realtimechatting/RealTimeChattingApplication.java
@@ -0,0 +1,13 @@
+package org.example.realtimechatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeChattingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeChattingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/realtimechatting/RealTimeChattingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.realtimechatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeChattingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
