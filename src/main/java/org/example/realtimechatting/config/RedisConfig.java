820b45f3b47ab2ba92252642f44bab9242388025
jeonyoungho <yhjun1000@gmail.com>
2025-05-11 14:40:35 +0900

    spring-boot-starter-web 의존성 제거 및 패키지 이동

--- src/main/java/org/example/realtimechatting/config/RedisConfig.java
+++ src/main/java/org/example/realtimechatting/config/RedisConfig.java
@@ -1,7 +1,7 @@
 package org.example.realtimechatting.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.example.realtimechatting.service.ChatMessageListener;
+import org.example.realtimechatting.stomp.ChatMessageListener;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/org/example/realtimechatting/stomp/ChatMessageListener.java
+++ src/main/java/org/example/realtimechatting/stomp/ChatMessageListener.java
@@ -1,4 +1,4 @@
-package org.example.realtimechatting.service;
+package org.example.realtimechatting.stomp;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
